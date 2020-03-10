27f033f2e331499bf858055b4fd0ec7c5be6b4fc
Rafael Malgor <rmalgor@itsynch.com>
2020-03-10 16:30:02 -0300

    mondays app deployed

---
+++ mondays/precache-manifest.972fe8f5102821423a51cfb20c934924.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9beb901d0fe7bf83b4c5b12a4a9bb2d4",
+    "url": "/mondays/index.html"
+  },
+  {
+    "revision": "fc0416748eb0a2f57f0d",
+    "url": "/mondays/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "9e6c48b47be8ab5b3e7c",
+    "url": "/mondays/static/css/main.26d7ce2b.chunk.css"
+  },
+  {
+    "revision": "fc0416748eb0a2f57f0d",
+    "url": "/mondays/static/js/2.ab7ac73f.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/mondays/static/js/2.ab7ac73f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e6c48b47be8ab5b3e7c",
+    "url": "/mondays/static/js/main.59b67984.chunk.js"
+  },
+  {
+    "revision": "e32f5f163ceafc5d6fb1",
+    "url": "/mondays/static/js/runtime-main.aa68e43a.js"
+  }
+]);
